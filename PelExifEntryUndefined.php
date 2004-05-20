c59013d1bd77ecc5674760b0c0fb9cb746bd0b1f
Martin Geisler <mgeisler@users.sourceforge.net>
2004-05-20 10:33:38 +0000

    Fix the FILE_SOURCE tag and add support for SCENE_TYPE.

--- PelExifEntryUndefined.php
+++ PelExifEntryUndefined.php
@@ -115,11 +115,20 @@ class PelExifEntryUndefined extends PelExifEntry {
     switch ($this->tag) {
     case PelExifTag::FILE_SOURCE:
       //CC (e->components, 1, v);
-      switch ($this->bytes{0}) {
+      switch (ord($this->bytes{0})) {
       case 0x03:
         return 'DSC';
       default:
-        return sprintf('0x%02X', $this->bytes{0});
+        return sprintf('0x%02X', ord($this->bytes{0}));
+      }
+   
+    case PelExifTag::SCENE_TYPE:
+      //CC (e->components, 1, v);
+      switch (ord($this->bytes{0})) {
+      case 0x01:
+        return 'Directly photographed';
+      default:
+        return sprintf('0x%02X', ord($this->bytes{0}));
       }
    
     case PelExifTag::COMPONENTS_CONFIGURATION:
