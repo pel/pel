5d94c144277caea357402cf22efe4cb151676978
Martin Geisler <mgeisler@users.sourceforge.net>
2004-06-06 18:54:14 +0000

    Added doc comments.

--- PelFormat.php
+++ PelFormat.php
@@ -51,19 +51,100 @@
  */
 class PelFormat {
 
+  /**
+   * Unsigned byte.
+   *
+   * Each component will be an unsigned 8-bit integer with a value
+   * between 0 and 255.
+   */
   const BYTE       =  1;
+  
+  /**
+   * ASCII string
+   *
+   * Each component will be an ASCII character.
+   */
   const ASCII      =  2;
+  
+  /**
+   * Unsigned short.
+   *
+   * Each component will be an unsigned 16-bit integer with a value
+   * between 0 and 65535.
+   */
   const SHORT      =  3;
+
+  /**
+   * Unsigned long.
+   *
+   * Each component will be an unsigned 32-bit integer with a value
+   * between 0 and 4294967295.
+   */
   const LONG       =  4;
+
+  /**
+   * Unsigned rational number.
+   *
+   * Each component will consist of two unsigned 32-bit integers
+   * denoting the enumerator and denominator.  Each integer will have
+   * a value between 0 and 4294967295.
+   */
   const RATIONAL   =  5;
+
+  /**
+   * Signed byte.
+   *
+   * Each component will be a signed 8-bit integer with a value
+   * between -128 and 127.
+   */
   const SBYTE      =  6;
+
+  /**
+   * Undefined byte.
+   *
+   * Each component will be a byte with no associated interpretation.
+   */
   const UNDEFINED  =  7;
+
+  /**
+   * Signed short.
+   *
+   * Each component will be a signed 16-bit integer with a value
+   * between -32768 and 32767.
+   */
   const SSHORT     =  8;
+
+  /**
+   * Signed long.
+   *
+   * Each component will be a signed 32-bit integer with a value
+   * between -2147483648 and 2147483647.
+   */
   const SLONG      =  9;
+
+  /**
+   * Signed rational number.
+   *
+   * Each component will consist of two signed 32-bit integers
+   * denoting the enumerator and denominator.  Each integer will have
+   * a value between -2147483648 and 2147483647.
+   */
   const SRATIONAL  = 10;
+
+  /** Floating point number. */
   const FLOAT      = 11;
+  /** Double precision floating point number. */
   const DOUBLE     = 12;
 
+
+  /**
+   * Returns the name of a format.
+   *
+   * @param PelFormat the format.
+   *
+   * @return string the name of the format, e.g., 'Ascii' for the
+   * {@link ASCII} format etc.
+   */
   static function getName($type) {
     switch ($type) {
     case self::ASCII:     return Pel::tra('Ascii');
@@ -82,6 +163,15 @@ class PelFormat {
     }
   }
 
+
+  /**
+   * Return the size of  components in a given format.
+   *
+   * @param PelFormat the format.
+   *
+   * @return the size in bytes needed to store one component with the
+   * given format.
+   */
   static function getSize($type) {
     switch ($type) {
     case self::ASCII:     return 1;
@@ -99,5 +189,6 @@ class PelFormat {
     default:              return Pel::fmt('Unknown type: 0x%X', $type);
     }
   }
+
 }
 ?>
