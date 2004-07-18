f6cbf01345bd09c2b8e943aa2bc3497a7726b966
Martin Geisler <mgeisler@users.sourceforge.net>
2004-07-18 12:37:43 +0000

    Updated tests to match updated image.  There are now 15 test cases with 727 individual tests in the test suite.

--- test/canon-ixus-ii.php
+++ test/canon-ixus-ii.php
@@ -74,48 +74,13 @@ class CanonIxusII extends UnitTestCase {
 
     $entry = $ifd0->getEntry(PelTag::DATE_TIME);
     $this->assertIsA($entry, 'PelEntryTime');
-    $this->assertEqual($entry->getValue(), gmmktime(16, 40, 8, 2, 9, 2004));
-    $this->assertEqual($entry->getText(), '2004:02:09 16:40:08');
+    $this->assertEqual($entry->getValue(), gmmktime(17, 43, 48, 7, 10, 2004));
+    $this->assertEqual($entry->getText(), '2004:07:10 17:43:48');
  
     $entry = $ifd0->getEntry(PelTag::YCBCR_POSITIONING);
     $this->assertIsA($entry, 'PelEntryShort');
     $this->assertEqual($entry->getValue(), 1);
     $this->assertEqual($entry->getText(), 'centered');
-
-    $entry = $ifd0->getEntry(PelTag::RELATED_IMAGE_WIDTH);
-    $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 1024);
-    $this->assertEqual($entry->getText(), '1024');
-
-    $entry = $ifd0->getEntry(PelTag::RELATED_IMAGE_LENGTH);
-    $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 768);
-    $this->assertEqual($entry->getText(), '768');
-
-    $entry = $ifd0->getEntry(PelTag::CUSTOM_RENDERED);
-    $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 0);
-    $this->assertEqual($entry->getText(), 'Normal process');
-
-    $entry = $ifd0->getEntry(PelTag::EXPOSURE_MODE);
-    $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 0);
-    $this->assertEqual($entry->getText(), 'Auto exposure');
-
-    $entry = $ifd0->getEntry(PelTag::WHITE_BALANCE);
-    $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 0);
-    $this->assertEqual($entry->getText(), 'Auto white balance');
-
-    $entry = $ifd0->getEntry(PelTag::DIGITAL_ZOOM_RATIO);
-    $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(2048, 2048));
-    $this->assertEqual($entry->getText(), '2048/2048');
-
-    $entry = $ifd0->getEntry(PelTag::SCENE_CAPTURE_TYPE);
-    $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 0);
-    $this->assertEqual($entry->getText(), 'Standard');
 
 
     /* The EXIF sub IFD. */
@@ -124,13 +89,13 @@ class CanonIxusII extends UnitTestCase {
 
     $entry = $exif->getEntry(PelTag::EXPOSURE_TIME);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(1, 640));
-    $this->assertEqual($entry->getText(), '1/640 sec.');
+    $this->assertEqual($entry->getValue(), array(1, 30));
+    $this->assertEqual($entry->getText(), '1/30 sec.');
 
     $entry = $exif->getEntry(PelTag::FNUMBER);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(71, 10));
-    $this->assertEqual($entry->getText(), 'f/7.1');
+    $this->assertEqual($entry->getValue(), array(32, 10));
+    $this->assertEqual($entry->getText(), 'f/3.2');
 
     $entry = $exif->getEntry(PelTag::EXIF_VERSION);
     $this->assertIsA($entry, 'PelEntryVersion');
@@ -139,13 +104,13 @@ class CanonIxusII extends UnitTestCase {
 
     $entry = $exif->getEntry(PelTag::DATE_TIME_ORIGINAL);
     $this->assertIsA($entry, 'PelEntryTime');
-    $this->assertEqual($entry->getValue(), gmmktime(16, 40, 8, 2, 9, 2004));
-    $this->assertEqual($entry->getText(), '2004:02:09 16:40:08');
+    $this->assertEqual($entry->getValue(), gmmktime(17, 43, 48, 7, 10, 2004));
+    $this->assertEqual($entry->getText(), '2004:07:10 17:43:48');
  
     $entry = $exif->getEntry(PelTag::DATE_TIME_DIGITIZED);
     $this->assertIsA($entry, 'PelEntryTime');
-    $this->assertEqual($entry->getValue(), gmmktime(16, 40, 8, 2, 9, 2004));
-    $this->assertEqual($entry->getText(), '2004:02:09 16:40:08');
+    $this->assertEqual($entry->getValue(), gmmktime(17, 43, 48, 7, 10, 2004));
+    $this->assertEqual($entry->getText(), '2004:07:10 17:43:48');
  
     $entry = $exif->getEntry(PelTag::COMPONENTS_CONFIGURATION);
     $this->assertIsA($entry, 'PelEntryUndefined');
@@ -154,23 +119,28 @@ class CanonIxusII extends UnitTestCase {
 
     $entry = $exif->getEntry(PelTag::COMPRESSED_BITS_PER_PIXEL);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(5, 1));
-    $this->assertEqual($entry->getText(), '5/1');
+    $this->assertEqual($entry->getValue(), array(2, 1));
+    $this->assertEqual($entry->getText(), '2/1');
+
+    $entry = $exif->getEntry(PelTag::SHUTTER_SPEED_VALUE);
+    $this->assertIsA($entry, 'PelEntrySRational');
+    $this->assertEqual($entry->getValue(), array(157, 32));
+    $this->assertEqual($entry->getText(), '157/32 sec. (APEX: 5)');
 
     $entry = $exif->getEntry(PelTag::APERTURE_VALUE);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(181, 32));
-    $this->assertEqual($entry->getText(), 'f/7.1');
+    $this->assertEqual($entry->getValue(), array(107, 32));
+    $this->assertEqual($entry->getText(), 'f/3.2');
 
     $entry = $exif->getEntry(PelTag::EXPOSURE_BIAS_VALUE);
     $this->assertIsA($entry, 'PelEntrySRational');
-    $this->assertEqual($entry->getValue(), array(0, 3));
-    $this->assertEqual($entry->getText(), '0');
+    $this->assertEqual($entry->getValue(), array(-1, 3));
+    $this->assertEqual($entry->getText(), '-0.3');
 
     $entry = $exif->getEntry(PelTag::MAX_APERTURE_VALUE);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(95, 32));
-    $this->assertEqual($entry->getText(), '95/32');
+    $this->assertEqual($entry->getValue(), array(107, 32));
+    $this->assertEqual($entry->getText(), '107/32');
 
     $entry = $exif->getEntry(PelTag::METERING_MODE);
     $this->assertIsA($entry, 'PelEntryShort');
@@ -185,8 +155,8 @@ class CanonIxusII extends UnitTestCase {
 
     $entry = $exif->getEntry(PelTag::FOCAL_LENGTH);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(173, 32));
-    $this->assertEqual($entry->getText(), '5.4 mm');
+    $this->assertEqual($entry->getValue(), array(215, 32));
+    $this->assertEqual($entry->getText(), '6.7 mm');
 
     $entry = $exif->getEntry(PelTag::MAKER_NOTE);
     $this->assertIsA($entry, 'PelEntryUndefined');
@@ -200,7 +170,6 @@ class CanonIxusII extends UnitTestCase {
     $this->assertEqual($entry->getValue(), 1.0);
     $this->assertEqual($entry->getText(), 'FlashPix Version 1.0');
 
-
     $entry = $exif->getEntry(PelTag::COLOR_SPACE);
     $this->assertIsA($entry, 'PelEntryShort');
     $this->assertEqual($entry->getValue(), 1);
@@ -208,23 +177,23 @@ class CanonIxusII extends UnitTestCase {
 
     $entry = $exif->getEntry(PelTag::PIXEL_X_DIMENSION);
     $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 768);
-    $this->assertEqual($entry->getText(), '768');
+    $this->assertEqual($entry->getValue(), 640);
+    $this->assertEqual($entry->getText(), '640');
 
     $entry = $exif->getEntry(PelTag::PIXEL_Y_DIMENSION);
     $this->assertIsA($entry, 'PelEntryShort');
-    $this->assertEqual($entry->getValue(), 1024);
-    $this->assertEqual($entry->getText(), '1024');
+    $this->assertEqual($entry->getValue(), 480);
+    $this->assertEqual($entry->getText(), '480');
 
     $entry = $exif->getEntry(PelTag::FOCAL_PLANE_X_RESOLUTION);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(768000, 156));
-    $this->assertEqual($entry->getText(), '768000/156');
+    $this->assertEqual($entry->getValue(), array(640000, 208));
+    $this->assertEqual($entry->getText(), '640000/208');
 
     $entry = $exif->getEntry(PelTag::FOCAL_PLANE_Y_RESOLUTION);
     $this->assertIsA($entry, 'PelEntryRational');
-    $this->assertEqual($entry->getValue(), array(1024000, 208));
-    $this->assertEqual($entry->getText(), '1024000/208');
+    $this->assertEqual($entry->getValue(), array(480000, 156));
+    $this->assertEqual($entry->getText(), '480000/156');
 
     $entry = $exif->getEntry(PelTag::FOCAL_PLANE_RESOLUTION_UNIT);
     $this->assertIsA($entry, 'PelEntryShort');
@@ -240,6 +209,56 @@ class CanonIxusII extends UnitTestCase {
     $this->assertIsA($entry, 'PelEntryUndefined');
     $this->assertEqual($entry->getValue(), chr(3));
     $this->assertEqual($entry->getText(), 'DSC');
+
+    $entry = $exif->getEntry(PelTag::CUSTOM_RENDERED);
+    $this->assertIsA($entry, 'PelEntryShort');
+    $this->assertEqual($entry->getValue(), 0);
+    $this->assertEqual($entry->getText(), 'Normal process');
+
+    $entry = $exif->getEntry(PelTag::EXPOSURE_MODE);
+    $this->assertIsA($entry, 'PelEntryShort');
+    $this->assertEqual($entry->getValue(), 1);
+    $this->assertEqual($entry->getText(), 'Manual exposure');
+
+    $entry = $exif->getEntry(PelTag::WHITE_BALANCE);
+    $this->assertIsA($entry, 'PelEntryShort');
+    $this->assertEqual($entry->getValue(), 1);
+    $this->assertEqual($entry->getText(), 'Manual white balance');
+
+    $entry = $exif->getEntry(PelTag::DIGITAL_ZOOM_RATIO);
+    $this->assertIsA($entry, 'PelEntryRational');
+    $this->assertEqual($entry->getValue(), array(2048, 2048));
+    $this->assertEqual($entry->getText(), '2048/2048');
+
+    $entry = $exif->getEntry(PelTag::SCENE_CAPTURE_TYPE);
+    $this->assertIsA($entry, 'PelEntryShort');
+    $this->assertEqual($entry->getValue(), 0);
+    $this->assertEqual($entry->getText(), 'Standard');
+
+
+    /* The Interoperability IFD. */
+    $iopr = $exif->getSubIfd(PelTag::INTEROPERABILITY_IFD_POINTER);
+    $this->assertNotNull($iopr);
+
+    $entry = $iopr->getEntry(PelTag::INTEROPERABILITY_INDEX);
+    $this->assertIsA($entry, 'PelEntryAscii');
+    $this->assertEqual($entry->getValue(), 'R98');
+    $this->assertEqual($entry->getText(), 'R98');
+
+    $entry = $iopr->getEntry(PelTag::INTEROPERABILITY_VERSION);
+    $this->assertIsA($entry, 'PelEntryVersion');
+    $this->assertEqual($entry->getValue(), 1);
+    $this->assertEqual($entry->getText(), 'Interoperability Version 1.0');
+
+    $entry = $iopr->getEntry(PelTag::RELATED_IMAGE_WIDTH);
+    $this->assertIsA($entry, 'PelEntryShort');
+    $this->assertEqual($entry->getValue(), 640);
+    $this->assertEqual($entry->getText(), '640');
+
+    $entry = $iopr->getEntry(PelTag::RELATED_IMAGE_LENGTH);
+    $this->assertIsA($entry, 'PelEntryShort');
+    $this->assertEqual($entry->getValue(), 480);
+    $this->assertEqual($entry->getText(), '480');
 
 
     /* The second IFD. */
