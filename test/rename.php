818b1c516d5f5ae47003dbd9de163505d5e46912
Martin Geisler <mgeisler@users.sourceforge.net>
2004-08-11 16:10:55 +0000

    A test script illustrating how PEL can be used to mass-rename files accourding to their timestamps.

---
+++ test/rename.php
@@ -0,0 +1,103 @@
+#!/usr/bin/php5
+<?php
+
+/*  PEL: PHP EXIF Library.  A library with support for reading and
+ *  writing all EXIF headers in JPEG and TIFF images using PHP.
+ *
+ *  Copyright (C) 2004  Martin Geisler <gimpster@users.sourceforge.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program in the file COPYING; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA 02111-1307 USA
+ */
+
+/* $Id$ */
+
+error_reporting(E_ALL);
+
+/* a printf() variant that appends a newline to the output. */
+function println(/* fmt, args... */) {
+    $args = func_get_args();
+    $fmt = array_shift($args);
+    vprintf($fmt . "\n", $args);
+}
+
+
+/* Make PEL speak the users language, if it is available. */
+setlocale(LC_ALL, '');
+
+require_once(dirname(__FILE__) . '/../PelDataWindow.php');
+require_once(dirname(__FILE__) . '/../PelJpeg.php');
+require_once(dirname(__FILE__) . '/../PelTiff.php');
+
+$prog = array_shift($argv);
+$error = false;
+
+if (isset($argv[0]) && $argv[0] == '-d') {
+  Pel::$debug = true;
+  array_shift($argv);
+}
+
+if (empty($argv)) {
+  println('Usage: %s [-d] <file> ...', $prog);
+  println('Optional arguments:');
+  println('  -d        turn debug output on.');
+  println('Mandatory arguments:');
+  println('  file ...  one or more file names.');
+  exit(1);
+}
+
+/* We typically need lots of RAM to parse TIFF images since they tend
+ * to be big and uncompressed. */
+ini_set('memory_limit', '32M');
+
+foreach ($argv as $file) {
+
+  println('Reading file "%s".', $file);
+  $data = new PelDataWindow(file_get_contents($file));
+
+  if (PelJpeg::isValid($data)) {
+    $jpeg = new PelJpeg();
+    $jpeg->load($data);
+    $app1 = $jpeg->getSection(PelJpegMarker::APP1);
+    $tiff = $app1->getTiff();
+  } elseif (PelTiff::isValid($data)) {
+    $tiff = new PelTiff($data);
+  } else {
+    println('Unrecognized image format! Skipping.');
+    continue;
+  }
+  
+  $ifd0 = $tiff->getIfd();
+  $entry = $ifd0->getEntry(PelTag::DATE_TIME);
+
+  if ($entry == null) {
+    println('Skipping %s because no DATE_TIME tag was found.', $file);
+    continue;
+  }
+
+  
+  $time = $entry->getValue();
+  
+  do {
+    $new = gmdate('Y:m:d-H:i:s', $time) . strchr($file, '.');
+    println('Trying file name %s', $new);
+    $time++;
+  } while (file_exists($new));
+  
+  rename($file, $new);
+}
+
+
+?>
