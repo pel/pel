92579650fbf805704b108e538aff8d6a6b18d2c4
Martin Geisler <mgeisler@users.sourceforge.net>
2006-09-07 22:29:34 +0000

    Updated to work with SVN instead of CVS.

--- make-release.sh
+++ make-release.sh
@@ -42,88 +42,118 @@
 # http://scripts.com/php-scripts/image-manipulation-scripts/pel-php-exif-library
 
 
-MAJOR=0
-MINOR=9
-VERSION=$MAJOR.$MINOR
+# Grab version number from ChangeLog...
+PREFIX='PEL Version'
+MIDDLE='[0-9]+\.[0-9]+(\.[0-9]+(-[0-9a-z]+)?)?'
+DATE=$(date -u '+%B %-d.. %Y')
+REGEXP="$PREFIX $MIDDLE  $DATE"
+
+if OFFSET=$(grep -n -E -m 1 "$REGEXP" NEWS | cut -d ':' -f 1); then
+    VERSION=$(head -n $OFFSET NEWS | tail -n 1 | cut -d ' ' -f 3)
+    echo "Found match for today in NEWS: $VERSION."
+
+    LINE=$(head -n $OFFSET NEWS | tail -n 1)
+    STARS=$(head -n $((OFFSET+1)) NEWS | tail -n 1)
+    if [ ${#LINE} != ${#STARS} ]; then
+        echo "Aborting because of bad underlining:"
+        echo
+        echo "$LINE"
+        echo "$STARS"
+        exit
+    fi
+else
+    echo "Found no version from today in NEWS, creating SVN version."
+    VERSION='svn' #$(date -u '+svn-%Y-%m-%d')
+fi
+
+exit
+
+# Determine if this is the final run or just a trial
+read -p "Create SVN tag and upload files to SourceForge? [y/N] " -n 1
+echo
+
+if [ $REPLY == "y" -a $VERSION != "svn" ]; then
+    SVN_URL="tags/$VERSION"
+else
+    SVN_URL="trunk"
+fi
 
 # Remove old directories, if present
 if test -e pel-$VERSION; then
-    echo "Removing old pel-$VERSION directory"
-    rm -r pel-$VERSION       \
-        pel-$VERSION.tar.bz2 \
-        pel-$VERSION.tar.gz  \
-        pel-$VERSION.zip
+    echo "Removing old pel-$VERSION directory and files"
+    rm -r pel-$VERSION pel-$VERSION.{tar.bz2,tar.gz,zip}
 fi
 
 if test -e image-tests; then
-    echo "Removing old pel-image-tests directory"
-    rm -r image-tests                    \
-        pel-image-tests-$VERSION.tar.bz2 \
-        pel-image-tests-$VERSION.tar.gz  \
-        pel-image-tests-$VERSION.zip
+    echo "Removing old pel-image-tests directory and files"
+    rm -r image-tests pel-image-tests-$VERSION.{tar.bz2,tar.gz,zip}
 fi
 
 
-# Determine if this is the final run or just a trial
-read -p "Tag CVS with 'release-${MAJOR}_${MINOR}' and upload files to SourceForge? [y/N] " -n 1
-echo
-
-# Do the tagging of CVS
-if [ "$REPLY" == "y" -o "$REPLY" == "Y" ]; then
-    echo -n "Tagging CVS with 'release-${MAJOR}_${MINOR}'... "
-    cvs -Q tag release-${MAJOR}_${MINOR}
-    echo "done."
+if [ $VERSION != 'svn' ]; then
+    if [ $SVN_URL != "trunk" ]; then
+        echo -n "Creating SVN tag 'pel-$VERSION'... "
+        svn copy https://svn.sourceforge.net/svnroot/pel/trunk \
+                 https://svn.sourceforge.net/svnroot/pel/$SVN_URL \
+            -m "Tagging PEL version $VERSION."
+        echo "done."
+    else
+        echo "Skipping tagging."
+    fi
 else
-    echo "Skipping tagging."
+    echo "Skipping tagging since this is a SVN snapshot."
 fi
 
+echo -n "Exporting $SVN_URL from SourceForge into pel-$VERSION... "
+svn export https://svn.sourceforge.net/svnroot/pel/$SVN_URL pel-$VERSION || exit
+# Export with Windows line endings for zip files
+#svn export https://svn.sourceforge.net/svnroot/pel/$SVN_URL pel-$VERSION
 
-echo -n "Retrieving CVS snapshot from SourceForge... "
-cvs -Q -z3 export -kv -r HEAD -d pel-$VERSION pel
-echo "done."
+
+cd pel-$VERSION
 
 # Generate the ChangeLog, prefixed with a standard header
-echo -n "Generating CVS ChangeLog... "
+echo -n "Generating SVN ChangeLog... "
 echo "ChangeLog file for PEL: PHP Exif Library.  A library with support for
 reading and writing Exif headers in JPEG and TIFF images using PHP.
 
 Copyright (C) 2004, 2005, 2006  Martin Geisler.
 Licensed under the GNU GPL, see COPYING for details.
 
-" > pel-$VERSION/ChangeLog
-cvs2cl --global-opts -q  \
-    --tagdates           \
-    --usermap users      \
-    --utc                \
-    --stdout >> pel-$VERSION/ChangeLog
+" > ChangeLog || exit
+svn2cl \
+    --include-rev \
+    --group-by-day \
+    --separate-daylogs \
+    --reparagraph \
+    --authors=authors.xml \
+    --stdout \
+    --strip-prefix=trunk/
+    https://svn.sourceforge.net/svnroot/pel/ >> ChangeLog
 echo "done."
 
+#echo -n "Marking releases in ChangeLog... "
+#sed -re '/./{H;$!d;};x;/tags/s|tags/pel-([0-9]+\.[0-9]+).*|PEL Version \1|'
+#echo "done."
 
-cd pel-$VERSION
 
 # Generate the binary MO files
 ./update-locales.sh
 
 
 # Generate the API documentation
-./run-phpdoc.sh $VERSION '../../phpdocumentor/phpdoc'
+./run-phpdoc.sh $VERSION '../../../PhpDocumentor/phpdoc'
 
 
 # Cleanup files that aren't needed in the released package
-rm make-release.sh .cvsignore users
+rm make-release.sh users
 rm -r tutorials
 
 
 # Add anchors and headers to the HTML ChangeLog so that each release
 # notices can link back to it
 
-# My perl started to claim that it could not find the ChangeLog file?!
-# Use sed instead then...
-#
-#perl -pie 's|^\d{4}-\d\d-\d\d \d\d:\d\d  tag release-(\d)_(\d)$|</pre>\n\n<div align="center"><h2 id="v\1.\2">PEL Version \1.\2</h2></div>\n<pre>\n|' doc/ric_ChangeLog.html
-
-sed -i -re 's|^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}  tag release-([0-9])_([0-9])$|</pre>\n\n<div align="center"><h2 id="v\1.\2">PEL Version \1.\2</h2></div>\n\n<pre>\n|g' doc/ric_ChangeLog.html
-
+#sed -i -re 's|^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}  tag release-([0-9])_([0-9])$|</pre>\n\n<div align="center"><h2 id="v\1.\2">PEL Version \1.\2</h2></div>\n\n<pre>\n|g' doc/ric_ChangeLog.html
 
 # Leave the package directory
 cd ..
@@ -156,7 +186,7 @@ echo "done."
 
 
 # Upload the compressed files and API documentation, if allowed
-if [ "$REPLY" == "y" -o "$REPLY" == "Y" ]; then
+if [ $REPLY == "y" -o $REPLY == "Y" ]; then
     echo -n "Uploading files to SourceForge for release... "
     ncftpput upload.sourceforge.net /incoming \
         pel-$VERSION.tar.gz                   \
