9c9236d2a287de44a2708dd09e5e76a570569cd0
Brad Bell <brad@pixelandtonic.com>
2016-04-26 20:05:41 -0700

    Not sure if it's used anymore, but change the PEAR makepackagexml.php to use LGPL

--- makepackagexml.php
+++ makepackagexml.php
@@ -13,7 +13,6 @@
  * @package PEL
  * @author  Lars Olesen <lars@legestue.net>
  * @version @package-version@
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public
  */
 require_once 'PEAR/PackageFileManager2.php';
 
@@ -59,7 +58,7 @@ $pfm->setDescription('The PHP Exif Library (PEL) lets you fully manipulate Exif 
 
 Using PEL, one can fully modify the Exif data, meaning that it can be both read and written. Completely new Exif data can also be added to images. PEL is written completely in PHP and depends on nothing except a standard installation of PHP, version 5. PEL is hosted on SourceForge.');
 $pfm->setUri($url);
-$pfm->setLicense('GPL License', 'http://www.gnu.org/licenses/gpl.html');
+$pfm->setLicense('LGPL License', 'http://www.gnu.org/licenses/lgpl.html');
 $pfm->addMaintainer('lead', 'mgeisler', 'Martin Geisler', 'mgeisler@mgeisler.net');
 $pfm->addMaintainer('helper', 'lsolesen', 'Lars Olesen', 'lars@legestue.net');
 
