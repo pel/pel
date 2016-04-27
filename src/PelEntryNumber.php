b9f0e913018394a14e020009ceafe28aac2f5576
Brad Bell <brad@pixelandtonic.com>
2016-04-26 20:02:32 -0700

    Dual license. And remove redundant/inconsistent @license docblock in PHP files.

--- src/PelEntryNumber.php
+++ src/PelEntryNumber.php
@@ -7,20 +7,8 @@
  *
  * Copyright (C) 2004, 2005, 2006 Martin Geisler.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program in the file COPYING; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA 02110-1301 USA
+ * Dual licensed. For the full copyright and license information, please view
+ * the COPYING.LESSER and COPYING files that are distributed with this source code.
  */
 namespace lsolesen\pel;
 
@@ -28,8 +16,6 @@ namespace lsolesen\pel;
  * Abstract class for numbers.
  *
  * @author Martin Geisler <mgeisler@users.sourceforge.net>
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public
- *          License (GPL)
  * @package PEL
  */
 
