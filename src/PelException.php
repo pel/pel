f4cdb638600914e21577af5ae453c1c087c3681a
Johannes Weberhofer <jweberhofer@weberhofer.at>
2017-02-02 16:52:31 +0100

    Allow PelException without arguments

--- src/PelException.php
+++ src/PelException.php
@@ -53,7 +53,7 @@ class PelException extends \Exception
      *            any number of arguments to be used with
      *            the format string.
      */
-    public function __construct($fmt, $args)
+    public function __construct($fmt, $args = null)
     {
         $args = func_get_args();
         $fmt = array_shift($args);
