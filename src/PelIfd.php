635a365ca55fbd941d2313c0c59c772b58bc472e
mondrake <mondrake@mondrake.org>
2017-11-18 17:09:34 +0100

    Catch DataWindow exceptions in PelIfd (#114)
    Refactored tests
    
    * Fixes for #10
    
    * code style fixes
    
    * Cleanup of tests
    
    * wrong test file renamed
    
    * Right test image file

--- src/PelIfd.php
+++ src/PelIfd.php
@@ -344,8 +344,13 @@ class PelIfd implements \IteratorAggregate, \ArrayAccess
                     }
 
                     if ($starting_offset != $o) {
-                        $this->sub[$type] = new PelIfd($type);
-                        $this->sub[$type]->load($d, $o);
+                        $ifd = new PelIfd($type);
+                        try {
+                            $ifd->load($d, $o);
+                            $this->sub[$type] = $ifd;
+                        } catch (PelDataWindowOffsetException $e) {
+                            Pel::maybeThrow(new PelIfdException($e->getMessage()));
+                        }
                     } else {
                         Pel::maybeThrow(new PelIfdException('Bogus offset to next IFD: %d, same as offset being loaded from.', $o));
                     }
@@ -760,7 +765,11 @@ class PelIfd implements \IteratorAggregate, \ArrayAccess
             }
 
             /* Now set the thumbnail normally. */
-            $this->setThumbnail($d->getClone($offset, $length));
+            try {
+                $this->setThumbnail($d->getClone($offset, $length));
+            } catch (PelDataWindowWindowException $e) {
+                Pel::maybeThrow(new PelIfdException($e->getMessage()));
+            }
         }
     }
 
