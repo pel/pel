efbb9efe110e107a7dba8431d37af75a20e28023
Artem Shelest <artem.e.shelest@gmail.com>
2017-11-19 17:43:18 +0200

    Corrected the test

--- test/PelEntryWindowsStringTest.php
+++ test/PelEntryWindowsStringTest.php
@@ -37,10 +37,19 @@ class PelEntryWindowsStringTest extends TestCase
         $test_str = 'Tést';
         $test_str_ucs2 = mb_convert_encoding($test_str, 'UCS-2LE', 'auto');
         $test_str_ucs2_zt = $test_str_ucs2 . PelEntryWindowsString::ZEROES;
+
         $entry = new PelEntryWindowsString(PelTag::XP_TITLE, $test_str);
         $this->assertNotEquals($entry->getValue(), $entry->getBytes(PelConvert::LITTLE_ENDIAN));
         $this->assertEquals($entry->getValue(), $test_str);
         $this->assertEquals($entry->getBytes(PelConvert::LITTLE_ENDIAN), $test_str_ucs2_zt);
+
+        // correct zero-terminated data from the exif
+        $entry->setValue($test_str_ucs2_zt, true);
+        $this->assertNotEquals($entry->getValue(), $entry->getBytes(PelConvert::LITTLE_ENDIAN));
+        $this->assertEquals($entry->getValue(), $test_str);
+        $this->assertEquals($entry->getBytes(PelConvert::LITTLE_ENDIAN), $test_str_ucs2_zt);
+
+        // incorrect data from exif
         $entry->setValue($test_str_ucs2, true);
         $this->assertNotEquals($entry->getValue(), $entry->getBytes(PelConvert::LITTLE_ENDIAN));
         $this->assertEquals($entry->getValue(), $test_str);
