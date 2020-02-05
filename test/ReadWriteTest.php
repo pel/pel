1e4b50cdbeea825ac6544b4ee1517da3273e4f5c
mondrake <mondrake.org@gmail.com>
2020-02-05 23:30:25 +0100

    Update ReadWriteTest.php

--- test/ReadWriteTest.php
+++ test/ReadWriteTest.php
@@ -227,6 +227,8 @@ class WriteEntryTest extends TestCase
         $this->assertEquals(1, $orientation->getValue());
         $photometric_interpretation = $ifd->getEntry(PelTag::PHOTOMETRIC_INTERPRETATION);
         $this->assertEquals(2, $photometric_interpretation->getValue());
+        $bits_per_sample = $ifd->getEntry(PelTag::BITS_PER_SAMPLE);
+        $this->assertEquals(2, $bits_per_sample->getValue());       
 
         $orientation->setValue(4);
         $photometric_interpretation->setValue(4);
