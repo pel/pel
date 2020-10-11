8a338cfb57e26f21d4ba42098c269455ba143ad1
mondrake <mondrake.org@gmail.com>
2020-10-11 09:03:06 +0200

    Cleanup

--- autoload.php
+++ autoload.php
@@ -26,11 +26,11 @@
  * Register autoloader for pel
  */
 spl_autoload_register(function ($class) {
-   if (substr_compare($class, 'lsolesen\\pel\\', 0, 13) === 0) {
-       $classname = str_replace('lsolesen\\pel\\', '', $class);
-       $load = realpath(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $classname . '.php');
-       if ($load !== false) {
-           include_once realpath($load);
-       }
-   }
+    if (substr_compare($class, 'lsolesen\\pel\\', 0, 13) === 0) {
+        $classname = str_replace('lsolesen\\pel\\', '', $class);
+        $load = realpath(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $classname . '.php');
+        if ($load !== false) {
+            include_once realpath($load);
+        }
+    }
 });
