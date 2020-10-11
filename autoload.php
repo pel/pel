f6d60b0308a1bd4344ddb2c242c9a1206e9b2fa9
mondrake <mondrake.org@gmail.com>
2020-10-11 09:01:13 +0200

    Cleanup

--- autoload.php
+++ autoload.php
@@ -25,13 +25,12 @@
 /**
  * Register autoloader for pel
  */
- spl_autoload_register(function ($class) {
-     if (substr_compare($class, 'lsolesen\\pel\\', 0, 13) === 0) {
-         $classname = str_replace('lsolesen\\pel\\', '', $class);
-         $load = realpath(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $classname . '.php');
-         if ($load !== false) {
-             include_once realpath($load);
-         }
-     }
- });
- 
+spl_autoload_register(function ($class) {
+   if (substr_compare($class, 'lsolesen\\pel\\', 0, 13) === 0) {
+       $classname = str_replace('lsolesen\\pel\\', '', $class);
+       $load = realpath(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $classname . '.php');
+       if ($load !== false) {
+           include_once realpath($load);
+       }
+   }
+});
