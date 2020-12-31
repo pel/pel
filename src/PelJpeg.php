8070901df68fed05d79f863dbd4d1eb60de59a80
Johannes Weberhofer <jweberhofer@weberhofer.at>
2020-12-31 18:41:59 +0100

    Fixed a namespace

--- src/PelJpeg.php
+++ src/PelJpeg.php
@@ -493,7 +493,7 @@ class PelJpeg
      * @param integer $skip
      *            the number of sections to be skipped. This must be a
      *            non-negative integer.
-     * @return PelJpegContent|lsolesen\pel\PelExif the content found, or null if there is no
+     * @return PelJpegContent|\lsolesen\pel\PelExif the content found, or null if there is no
      *         content available.
      */
     public function getSection($marker, $skip = 0)
